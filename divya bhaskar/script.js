11b5770e7ce361dca1f64bbeb476f0b318d79ba0
Dilipbhai2525 <dilipbhaichaudhari2525@gmail.com>
2025-04-10 14:00:49 +0530

    dilited not intrasted

--- divya bhaskar/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.navbar li a').forEach(link => {
-  link.addEventListener('click', function() {
-      // પહેલાંની એક્ટિવ ક્લાસ હટાવો
-      document.querySelectorAll('.navbar li a').forEach(el => el.classList.remove('active'));
-      
-      // ક્લિક કરેલ લિંકને એક્ટિવ કરો
-      this.classList.add('active');
-  });
-});
